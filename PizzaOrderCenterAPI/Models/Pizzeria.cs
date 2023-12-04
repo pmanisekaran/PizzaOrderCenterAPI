7da9b09e515270ac7c650f3af22615995dfbbd81
Mani Periasamy <pmanisekaran@yahoo.com>
2023-12-04 19:22:34 +1100

    Added Pizzeria and its menu as wo models. Each pizzeria has name and location. Ideally one can add multiple pizzeria to a location too. Location is free text. Also installe din memory EF core

---
+++ PizzaOrderCenterAPI/Models/Pizzeria.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PizzaOrderCenterAPI.Models
+{
+	public class Pizzeria
+	{
+
+		[Required]
+		[Key]
+		public int PizzeriaId { get; set; }
+
+		[Required]
+		[MaxLength(255)]
+		public string PizzeriaName { get; set; }
+
+		[Required]
+		[MaxLength(255)]
+		public int Location { get; set; }
+
+		[Required]
+		ICollection<Pizza> Pizzas { get; set; }
+
+	}
+}
---
+++ PizzaOrderCenterAPI/Models/PizzeriaMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PizzaOrderCenterAPI.Models
+{
+	public class Pizza
+	{
+		[Key]
+		[Required]
+		public int PizzaId { get; set; }
+
+		[Required]
+		public string PizzaName { get; set; }
+
+		
+		[Required]
+		public int PizzeriaId { get; set; }
+
+	}
+}
